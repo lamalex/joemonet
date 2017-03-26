6c9f2c03681b23a568f7231c96940a5350803505
Alex Launi <alex.launi@gmail.com>
2017-03-26 11:53:20 -0400

    implement fading on the landing page. pretty slick

--- client/templates/landing/landing.js
+++ client/templates/landing/landing.js
@@ -3,3 +3,19 @@ Template.landing.helpers({
     return Session.get('resetPasswordToken');
   }
 });
+
+Template.layout.onRendered(function() {
+  this.find('#wrapper')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
+    }
+  }
+});
