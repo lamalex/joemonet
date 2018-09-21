43e55573c113d31fd867ce9eee32c4ce196055d7
Alex Launi <alex.launi@gmail.com>
2018-09-21 11:58:06 -0400

    Upgrade to Meteor 1.7.0.5

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import _ from 'lodash';
 
 Meteor.startup(() => {
-  process.env.MAIL_URL = Meteor.settings.private.MAIL_URL;
+  process.env.MAIL_URL = _.get(Meteor.settings, "private.MAIL_URL", "");
 });
